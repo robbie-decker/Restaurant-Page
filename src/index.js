292385bdb6955b3d2d8d4dadc1b6f6696db458ac
Robbie Decker <robbd56@gmail.com>
2023-09-06 21:55:01 -0400

    Added some css decor and got working content swap

--- src/index.js
+++ src/index.js
@@ -1,9 +1,55 @@
 import './style.css';
+import yellowBorder from './yellow_border.svg';
 
 const content = document.getElementById("content");
 
-const p = document.createElement('p');
+const currentContent = document.createElement('div');
 
-p.textContent = "This is cool";
-// p.classList.add("text");
-content.appendChild(p);
+
+const homeHTML = `
+    <div class="top_seperator"></div>
+    <div class="bottom_seperator"></div>
+    <div class="cut_container"></div>
+    <div class="cut_container" style="width: 42px;"></div>
+    <div class="black_container"></div>
+    <div class="gallery_container">
+        <span class= "gallery_item_arrow"></span>
+        <div class="myImg"></div>
+    </div>
+    <img class=yellow_border src=${yellowBorder} alt="Border Image">
+`;
+
+const menuHTML = `
+    <p>Here is the menu. Yum! </p>
+`;
+const contactHTML = `
+    <p>Here are some people</p>
+`;
+
+
+currentContent.innerHTML = homeHTML;
+
+content.appendChild(currentContent);
+
+const header = document.getElementById("nav_menu");
+for(let link of header.children){
+    link.addEventListener('click', () => {
+        // Want to stop from running if the user reclicks the same button
+        console.log("this is happening");
+        content.removeChild(currentContent);
+        switch (link.id){
+            case "home":
+                currentContent.innerHTML = homeHTML;
+                break;
+            case "menu":
+                currentContent.innerHTML = menuHTML;
+                break;
+            case "contact":
+                currentContent.innerHTML = contactHTML;
+                break;
+        }
+        content.appendChild(currentContent);     
+    });
+}
+
+
