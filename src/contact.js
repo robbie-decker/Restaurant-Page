09bdf51cb1e02e146b65c7412e6fd87b8d4cf5a4
Robbie Decker <robbd56@gmail.com>
2023-09-08 23:45:02 -0400

    Overhauls contact page
    Thinking of renaming this to the staff page

--- src/contact.js
+++ src/contact.js
@@ -7,20 +7,32 @@ const contactHTML = `
     <div class="top_seperator"></div>
     <div id="contact_gallery">
         <div class="contact_container">
-            <p>Johnny SilverHand</p>
             <img src="${images['Johnny_Silverhand.webp'].default}" alt="Johnny Silverhand picture">
+            <div class="overlay">
+                <h4>Johnny SilverHand</h4>
+                <p>- Blurb about Johnny</p>
+            </div>
         </div>
         <div class="contact_container">
-            <p>Judy Alverez</p>
             <img src="${images['Judy_Alvarez.webp'].default}" alt="Judy Alvarez Picture">
+            <div class="overlay">
+                <h4>Judy Alverez</h4>
+                <p>- Blurb about Judy</p>
+            </div>
         </div>
         <div class="contact_container">
-            <p>Rogue Amendiares</p>
             <img src="${images['Rogue.webp'].default}" alt="Rogue Amendiares Picture">
+            <div class="overlay">
+                <h4>Rogue Amendiares</h4>
+                <p>- Blurb about Rogue</p>
+            </div>
         </div>
         <div class="contact_container">
-            <p>Claire Russell</p>
             <img src="${images['ClaireRussell.webp'].default}" alt="Claire Russell Picture">
+            <div class="overlay">
+                <h4>Claire Russell</h4>
+                <p>- Blurb about Claire</p>
+            </div>
         </div>
     </div>
     <div class="bottom_seperator"></div>
