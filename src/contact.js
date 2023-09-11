3093b56981e0ab838febc9d92324c401c7195f39
Robbie Decker <robbd56@gmail.com>
2023-09-11 10:17:39 -0400

    Fix images not loadin since changing asset loading

--- src/contact.js
+++ src/contact.js
@@ -7,28 +7,28 @@ const contactHTML = `
     <div class="top_seperator"></div>
     <div id="contact_gallery">
         <div class="contact_container">
-            <img src="${images['Johnny_Silverhand.webp'].default}" alt="Johnny Silverhand picture">
+            <img src="${images['Johnny_Silverhand.webp']}" alt="Johnny Silverhand picture">
             <div class="overlay">
                 <h4>Johnny SilverHand</h4>
                 <p>- Blurb about Johnny</p>
             </div>
         </div>
         <div class="contact_container">
-            <img src="${images['Judy_Alvarez.webp'].default}" alt="Judy Alvarez Picture">
+            <img src="${images['Judy_Alvarez.webp']}" alt="Judy Alvarez Picture">
             <div class="overlay">
                 <h4>Judy Alverez</h4>
                 <p>- Blurb about Judy</p>
             </div>
         </div>
         <div class="contact_container">
-            <img src="${images['Rogue.webp'].default}" alt="Rogue Amendiares Picture">
+            <img src="${images['Rogue.webp']}" alt="Rogue Amendiares Picture">
             <div class="overlay">
                 <h4>Rogue Amendiares</h4>
                 <p>- Blurb about Rogue</p>
             </div>
         </div>
         <div class="contact_container">
-            <img src="${images['ClaireRussell.webp'].default}" alt="Claire Russell Picture">
+            <img src="${images['ClaireRussell.webp']}" alt="Claire Russell Picture">
             <div class="overlay">
                 <h4>Claire Russell</h4>
                 <p>- Blurb about Claire</p>
--- src/menu.js
+++ src/menu.js
@@ -1,7 +1,6 @@
 import {importAll} from "./index.js";
 
 const images = importAll(require.context('./imgs/food_and_drink', false, /\.(png|jpe?g|svg|webp)$/));
-console.log(images);
 const menuHTML = `
 <h1>Menu</h1>
     <div id="drinks">
@@ -10,21 +9,21 @@ const menuHTML = `
         <div class="food_item">
             <div class="border">
                 <p><strong>Johnny SilverHand</strong></p>
-                <img src="${images['johnny.webp'].default}" alt="Johnny Silverhand drink">
+                <img src="${images['johnny.webp']}" alt="Johnny Silverhand drink">
                 <p class="food_description">- A tequila old fashioned with a splash of beer and a chili garnish. For the rockerboy who never gave up.</p>
             </div>
         </div>
         <div class="food_item">
             <div class="border">
                 <p>David Martinez</p>
-                <img src="${images['david.webp'].default}" alt="David Martinez drink">
+                <img src="${images['david.webp']}" alt="David Martinez drink">
                 <p class="food_description">- A shot of vodka on the rocks, topped with Nicola. Aim high and go out with a bang</p>
             </div>
         </div>
         <div class="food_item">
             <div class="border">
                 <p>NiCola</p>
-                <img src="${images['nicola.webp'].default}" alt="NiCola picture">
+                <img src="${images['nicola.webp']}" alt="NiCola picture">
                 <p class="food_description">- America's favorite drink!</p>
             </div>
         </div>
@@ -35,21 +34,21 @@ const menuHTML = `
         <div class="food_item">
             <div class="border">
                 <p>Artichoke ‘N’ Avocado Pizza</p>
-                <img src="${images['artichoke_pizza.webp'].default}" alt="Artichoke ‘N’ Avocado Pizza">
+                <img src="${images['artichoke_pizza.webp']}" alt="Artichoke ‘N’ Avocado Pizza">
                 <p class="food_description">- Topping it to the next level</p>
             </div>
         </div>
         <div class="food_item">
             <div class="border">
                 <p>Tofu’d Tuna and Pineapple Pizza</p>
-                <img src="${images['tofu_pizza.webp'].default}" alt="Tofu’d Tuna and Pineapple Pizza">
+                <img src="${images['tofu_pizza.webp']}" alt="Tofu’d Tuna and Pineapple Pizza">
                 <p class="food_description">- Vegans can’t get enough of it</p>
             </div>
         </div>
         <div class="food_item">
             <div class="border">
                 <p>Locust Pepperoni Pizza</p>
-                <img src="${images['locust_pizza.webp'].default}" alt="Locust Pepperoni Pizza">
+                <img src="${images['locust_pizza.webp']}" alt="Locust Pepperoni Pizza">
                 <p class="food_description">- It’ll swarm your taste buds!</p>
             </div>
         </div>
@@ -57,35 +56,35 @@ const menuHTML = `
         <div class="food_item">
             <div class="border">
                 <p>Eezybeef<p>
-                <img src="${images['eezybeef.webp'].default}" alt="Eezybeef picture. Looks kinda gross">
+                <img src="${images['eezybeef.webp']}" alt="Eezybeef picture. Looks kinda gross">
                 <p class="food_description">Synthetic meat - you’re in for a treat!</p>
             </div>
         </div>
         <div class="food_item">
             <div class="border">
                 <p>Hawt Dawg<p>
-                <img src="${images['hawt_dawg.webp'].default}" alt="Hawt Dawg picture">
+                <img src="${images['hawt_dawg.webp']}" alt="Hawt Dawg picture">
                 <p class="food_description">The thickest dogs and tightest buns in town!</p>
             </div>      
         </div>
         <div class="food_item">
             <div class="border">
                 <p>RealFruit<p>
-                <img src="${images['realfruit.webp'].default}" alt="RealFruit picture">
+                <img src="${images['realfruit.webp']}" alt="RealFruit picture">
                 <p class="food_description">It's not marketing. It's real.</p>
             </div>
         </div>
         <div class="food_item">
             <div class="border">
                 <p>Rammmmen<p>
-                <img src="${images['rammmmen.webp'].default}" alt="Rammmen Picture. Looks like rammen">
+                <img src="${images['rammmmen.webp']}" alt="Rammmen Picture. Looks like rammen">
                 <p class="food_description">Brand new recipe! 3X more MSG!</p>
             </div>
         </div>
         <div class="food_item">
             <div class="border">
                 <p>Shwabshwab (Fried Ants) Ketchup<p>
-                <img src="${images['fried_ants.webp'].default}" alt="Fried Ants picture">
+                <img src="${images['fried_ants.webp']}" alt="Fried Ants picture">
                 <p class="food_description">Your favorite food flavored with your second favorite food</p>
             </div>
         </div>
